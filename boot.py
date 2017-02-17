2f78fc864d4837275cd4b5531f83b54bfdcdeb19
Ben gift <benhgift@gmail.com>
2017-02-17 11:36:09 -0800

    renamed main and added error handling in boot

--- boot.py
+++ boot.py
@@ -1,33 +1,39 @@
-import sys
-import esp                                                                                             
-import gc                                                                                               
-import webrepl                                                                                          
-from my_main import *
+# By wrapping the whole thing in a try clause we avoid having
+# to reflash the firmware if there's a bug
+try:
+    import sys
+    import esp                                                                                             
+    import gc                                                                                               
+    import webrepl                                                                                          
+    from main import *
+    import main
 
-esp.osdebug(None)                                                                                      
-webrepl.start()                                                                                         
-gc.collect()
-
-
-def readfile(file="boot.py"):
-    with open(file,'r') as thefile:
-        for line in thefile:
-            print(line, end='')
-    print()
+    esp.osdebug(None)                                                                                      
+    webrepl.start()                                                                                         
+    gc.collect()
 
 
-def wipe_module(module):
-    del sys.modules[module]
+    def readfile(file="boot.py"):
+        with open(file,'r') as thefile:
+            for line in thefile:
+                print(line, end='')
+        print()
 
 
-def rel():
-    try:
-        wipe_module('my_main')
-        wipe_module('test_all')
-        wipe_module('pixel')
-        wipe_module('timing')
-        wipe_module('config')
-    except:
-        pass
-    from my_main import *
-    from test_all import *
+    def wipe_module(module):
+        del sys.modules[module]
+
+
+    def rel():
+        try:
+            wipe_module('main')
+            wipe_module('test_all')
+            wipe_module('pixel')
+            wipe_module('timing')
+            wipe_module('config')
+        except:
+            pass
+        from main import *
+        from test_all import *
+except:
+    everything_is_on_fire = True
